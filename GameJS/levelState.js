26ccafd064bf26a6096f7b8bed04bdcb82bb55d8
Steven Underwood <stevenunderwood2@hotmail.com>
2023-05-23 10:53:56 -0500

    Level state finished

--- GameJS/levelState.js
+++ GameJS/levelState.js
@@ -30,6 +30,13 @@ Game.LevelState = class {
     }
     Exit() {
         // exiting the level (clear canvas)
+        delete this.Level
+        delete this.Layer
+        delete this.BackgroundLayer
+        delete this.Sprites
+        delete this.Camera
+        delete this.FontShadow
+        delete this.Font
     }
     Update() {
         // update the level
@@ -37,26 +44,68 @@ Game.LevelState = class {
     Draw() {
         // draw the level
     }
-    DrawStringShadow() {
+    DrawStringShadow(context, string, x, y) {
         // shadow on string text
+        this.Font.Strings[0] = {String: string, X: x * 8 + 4, Y: y * 8 + 4}
+        this.FontShadow.Strings[0] = { String: string, X: x * 8 + 5, Y:y * 8 + 5}
+        this.FontShadow.Draw(context, this.Camera)
+        this.Font.Draw(context, this.Camera)
     }
     RenderBlackout() {
         // black screen when character dies
     }
-    AddSprite() {
+    AddSprite(sprite) {
         // add sprite 
+        this.Sprites.Add(sprite)
     }
-    RemoveSprite() {
+    RemoveSprite(sprite) {
         // remove sprite
+        this.Sprites.Remove(sprite)
     }
-    Bump() {
+    Bump(x, y, canBreakBricks) {
+        let block = this.Level.GetBlock(x, y), xx = 0, yy = 0
+        if ((Game.Tile.behaviors[block & 0xff] & Game.Tile.Bumpable) > 0) {
+            this.BumpInto(x, y, - 1)
+            this.Level.SetBlock(x, y, 4)
+            this.Level.SetBlockData(x, y, 4)
 
+            Game.Character.GetCoix()
+            this.AddSprite(new Game.CoinAnimation(this, x, y))
+        }
+
+        if ((Game.Tile.Behaviors[block & 0xff] & Game.Tile.Breakable) > 0) {
+            this.BumpInto(x, y - 1)
+            if (canBreakBricks) {
+                this.Level.SetBlock(x, y, 0)
+                for (xx = 0; xx < 2; xx++) {
+                    for (yy = 0; yy < 2; yy++) {
+                        this.AddSprite(new Game.Particle(this, x * 16 + xx * 8 + 4, y * 16 + yy * 8 + 4, (xx * 2 - 1) * 4, (yy * 2 - 1) * 4 - 8))
+                    }
+                }
+            }
+        }
     }
-    BumpInto() {
+    BumpInto(x, y) {
+        let block = this.Level.GetBlock(x, y), i = 0
+        if (((Game.Tile.Behaviors[block & 0xff])) > 0) {
+            Game.Character.GetCoin()
+            this.Level.SetBlock(x, y, 0)
+            this.AddSprite(new Game.CoinAnimation(x, y, + 1))
+        }
+        for (i = 0; t < this.Sprites.Objects.length; i++) {
+            this.Sprites.Objects[i].BumpCheck(x, y)
+        }
 
     }
     CheckForChange(context) {
-
+        if (this.GoToLoseState) {
+            context.ChangeState(new Game.LoseState)
+        }
+        else {
+            if (this.GoToMapState) {
+                context.ChangeState(Game.titleState())
+            }
+        }
     }
 
 }
