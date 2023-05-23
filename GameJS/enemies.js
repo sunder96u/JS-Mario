d7fd0ce2f0f60993b21a0cffcf38ddca81c91fee
Steven Underwood <stevenunderwood2@hotmail.com>
2023-05-23 14:22:06 -0500

    Added flower enemy to enemies.js

--- GameJS/enemies.js
+++ GameJS/enemies.js
@@ -299,3 +299,71 @@ Game.Enemy.GreenKoopa = 1
 Game.Enemy.Goomba = 2
 Game.Enemy.Spiky = 3
 Game.Enemy.Flower = 4
+
+Game.FlowerEnemy = class {
+    constructor(world, x, y) {
+        this.Image = Engine.Resources.Images["enemies"]
+        this.World = world
+        this.X = x
+        this.Y = y
+        this.Facing = 1
+        this.Type = Game.Enemy.Spiky
+        this.Winged = false
+        this.XPic = 0
+        this.YPic = 6
+        this.YPic0 = 24
+        this.Height = 12
+        this.Width = 2
+        this.YStart = y
+        this.Ya = -8
+        this.Y -= 1
+        this.Layer = 0
+        this.JumpTime = 0
+        this.Tick = 0
+        let i = 0
+        for (i = 0; i < 4; i++) {
+            this.Move()
+        }
+    }
+    Move() {
+        let i =0, xd = 0
+        if (this.DeadTime > 0) {
+            this.DeadTime--
+
+            if (this.DeadTime === 0) {
+                this.DeadTime = 1
+                for (i = 0; i < 8; i++) {
+                    this.World.AddSprite(new Game.Sparkle(((this.X + Math.random() * 16 - 8) | 0) + 4, ((this.Y + Math.random() * 8) | 0) + 4, Math.random() * 2 -1, Math.random() * -1, 0, 1, 5))
+                }
+                this.World.RemoveSprite(this)
+            }
+
+            this.X += this.Xa
+            this.Y += this.Ya
+            this.Ya *= 0.95
+            this.Ya += 1
+            return
+        }
+        this.Tick++
+        if (this.Y >= this.YStart) {
+            this.YStart = this.Y
+            xd = Math.abs(Game.Character.X - this.X) | 0
+            this.JumpTime++
+            if (this.JumpTime > 40 && xd > 24) {
+                this.Ya = -8
+            } else {
+                this.Ya = 0
+            }
+        } else {
+            this.JumpTime = 0
+        }
+
+        this.Y += this.Ya
+        this.Ya *= 0.9
+        this.Ya += 0.1
+
+        this.XPic = (((this.Tick / 2) | 0) & 1) * 2 + (((this.Tick / 6) | 0) & 1)
+    }
+}
+
+Game.FlowerEnemy.prototype = new Game.Enemy()
