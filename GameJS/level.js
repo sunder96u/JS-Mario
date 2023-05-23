0e47ded6e13a6732badecd89f8b736ab640e07a9
Steven Underwood <stevenunderwood2@hotmail.com>
2023-05-23 09:35:32 -0500

    Level Renderer Added

--- GameJS/level.js
+++ GameJS/level.js
@@ -470,15 +470,59 @@ Game.LevelRender = class {
         this.Background = Game.SpriteCuts.GetLevelSheet()
     }
     Update(delta) {
+        this.AnimationTime += delta
+        this.Tick = this.AnimationTime | 0
+        this.Bounce += delta * 30
+        this.Delta = delta
 
     }
-    Draw() {
+    Draw(context, camera) {
+        this.DrawStatic(context, camera)
+        this.DrawDynamic(context, camera)
 
     }
-    DrawStatic() {
-
+    DrawStatic(context, camera) {
+        let x = 0, y = 0, b = 0, frame = null
+        let xTileStart = (camera.X / 16) | 0
+        let xTileEnd = ((camera.X + this.Width) / 16) | 0
+        for ( x = xTileStart; x < xTileEnd + 1; x++) {
+            for (y = 0; y < this.TilesY; y++) {
+                b = this.Level.GetBlock(x, y) & 0xff
+                if ((Game.Tile.Behaviors[b] & Game.Tile.Animated) === 0) {
+                    frame = this.Background[b % 16][(b / 16) | 0]
+                    context.drawImage(Engine.Resources.Images['map'], frame.X, frame.Y, frame.Width, frame.Height, ((x << 4) - camera.X) | 0, (y << 4) | 0, frame.Width, frame.Height)
+                }
+            }
+        }
     }
-    DrawDynamic() {
+    DrawDynamic(context, camera) {
+        let x = 0, y = 0, b = 0, animationTime = 0, yo = 0, frame = null
+        for (x = (camera.X / 16) | 0; x < ((camera.X + this.Width) / 16) | 0; x++) {
+            for (y = (camera.Y / 16) | 0; y <= ((camera.Y + this.Height) / 16) | 0; y++) {
+                b = this.Level.GetBlock(x, y)
+                if (((Game.Tile.Behaviors[b & 0xff]) & Game.Tile.Animated) > 0) {
+                    animationTime = ((this.Bounce / 3) | 0) % 4
+                    if ((((b % 16) / 4) | 0) === 0 && ((b / 16) | 0) === 1) {
+                        animationTime = ((this.Bounce / 2 (x + y) / 8) | 0) % 20
+                        if (animationTime > 3) {
+                            animationTime = 0
+                        }
+                    }
+                    if ((((b % 16) / 4) | 0) === 3 && ((b / 16) | 0) === 0) {
+                        animationTime = 2
+                    }
+                    yo = 0
+                    if (x >= 0 && y >= 0 && x < this.Level.Width && y < this.Level.Height) {
+                        yo = this.Level.Data[x][y]
+                    }
+                    if ( yo > 0) {
+                        yo = (Math.sin((yo - this.Delta) / 4 * Math.PI) * 8) | 0
+                    }
+                    frame = this.Background[(((b % 16) / 4) | 0) * 4 + animationTime][(b / 16) | 0]
+                    context.drawImage(Engine.Resources.Images["map"], frame.X, frame.Y, frame.Width, frame.Height, (x << 4) - camera.X, (y << 4) - camera.Y - yo, frame.Width, frame.Height)
+                }
+            }
+        }
 
     }
 }
